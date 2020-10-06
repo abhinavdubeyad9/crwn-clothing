b8d55385f29c45a7c9d1d17bb49b65224b09a11a
Abhinav Dubey <dubey,abhinav76@gmail.com>
2020-10-06 19:41:49 +0530

    Added sign in component and custom button component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-
+import signInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={signInAndSignUpPage} />
       </Switch>
     </div>
   );
