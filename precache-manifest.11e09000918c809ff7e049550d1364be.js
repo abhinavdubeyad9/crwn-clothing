70c357dcb8782b3cd56cded5f22e3839ceed86ab
Abhinav Dubey <dubey,abhinav76@gmail.com>
2020-10-17 16:06:17 +0530

    Updates

---
+++ precache-manifest.11e09000918c809ff7e049550d1364be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88e9b64ca0f5aeaf7b3cbcd3d999882",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "8e367f5fd299a2a9968f",
+    "url": "/crwn-clothing/static/css/main.b76c2bff.chunk.css"
+  },
+  {
+    "revision": "4b85b9e966c7f2c0f1df",
+    "url": "/crwn-clothing/static/js/2.6559dffa.chunk.js"
+  },
+  {
+    "revision": "8e367f5fd299a2a9968f",
+    "url": "/crwn-clothing/static/js/main.dc158731.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
